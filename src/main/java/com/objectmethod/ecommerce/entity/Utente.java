1cdfc49e9527a5ebfc46d07f6b67b06b534dc110
Kristian <a@a.a>
2022-03-17 11:31:38 +0100

    utente

---
+++ src/main/java/com/objectmethod/ecommerce/entity/Utente.java
@@ -0,0 +1,51 @@
+package com.objectmethod.ecommerce.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="user")
+public class Utente {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@Column(name="nome")
+	private String nome;
+	@Column(name="cognome")
+	private String cognome;
+	@Column(name="email")
+	private String email;
+	@Column(name="username")
+	private String username;
+	@Column(name="password")
+	private String password;
+	@Column(name="amministratore")
+	private Boolean amministratore;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name ="idadress")
+	private Indirizzo indirizzo;
+	
+	@OneToOne
+	@JoinColumn(name ="idcarrello")
+	private Carello carello;
+	
+	@OneToMany
+	@JoinColumn(name ="idacquisto")
+	private List<Acquisto> acquisti;
+}
