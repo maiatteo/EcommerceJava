6c7fee89a2cdf3e583272d6c68cd41bc89cb08f8
Kristian <a@a.a>
2022-03-17 16:50:08 +0100

    list

--- src/main/java/com/objectmethod/ecommerce/service/CarrelloService.java
+++ src/main/java/com/objectmethod/ecommerce/service/CarrelloService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.objectmethod.ecommerce.dto.CarrelloDto;
 import com.objectmethod.ecommerce.entity.Carrello;
+import com.objectmethod.ecommerce.mapper.CarrelloMapper;
 import com.objectmethod.ecommerce.repository.CarrelloRepository;
 
 public class CarrelloService {
@@ -13,9 +14,12 @@ public class CarrelloService {
 	@Autowired
 	CarrelloRepository carrelloRepo;
 	
+	@Autowired
+	CarrelloMapper carrelloMapper;
+	
 	public List<CarrelloDto> findAll(){
 		List<Carrello> lst = carrelloRepo.findAll();
-		List<CarrelloDto> dto = carrelloRepo.toDto(lst);
+		List<CarrelloDto> dto = carrelloMapper.toDto(lst);
 		return dto;
 	}
 
