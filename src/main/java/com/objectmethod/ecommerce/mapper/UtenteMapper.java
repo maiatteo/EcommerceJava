6c9cd20c0053b7d520779277ec815c5b55882b7b
Matteo <matteomaialetti@gmail.com>
2022-03-21 10:05:42 +0100

    password fix in toDto

--- src/main/java/com/objectmethod/ecommerce/mapper/UtenteMapper.java
+++ src/main/java/com/objectmethod/ecommerce/mapper/UtenteMapper.java
@@ -11,16 +11,20 @@ import com.objectmethod.ecommerce.entity.Utente;
 
 @Mapper(componentModel = "spring")
 public abstract class UtenteMapper {
+	
+	@Mapping(target = "password", ignore = true)
 	@Mapping(target= "idCarrello", source ="carrello.id")
 	@Mapping(target= "idIndirizzo", source ="indirizzoUte.id")
 	public abstract UtenteDto toDto(Utente entity);
+	
 	
 	@Mapping(target= "carrello.id", source ="idCarrello")
 	@Mapping(target= "indirizzoUte.id", source ="idIndirizzo")
 	public abstract Utente toModel(UtenteDto dto);
 
-	
+	@Mapping(target = "password", ignore = true)
 	public abstract List<UtenteDto> toDto(List<Utente> lst);
 	
+	@Mapping(target = "password", ignore = true)
 	public abstract UtenteTokenDto toDto(Utente entity,String token);
 }
