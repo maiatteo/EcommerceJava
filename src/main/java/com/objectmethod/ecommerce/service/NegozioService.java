0618093191842e259de9b20383d6423dc1819979
Talento <Talento@ASUS-2.home>
2022-03-21 12:32:04 +0100

    negozio service e cambio response wrapper

--- src/main/java/com/objectmethod/ecommerce/service/NegozioService.java
+++ src/main/java/com/objectmethod/ecommerce/service/NegozioService.java
@@ -3,11 +3,13 @@ package com.objectmethod.ecommerce.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 
 import com.objectmethod.ecommerce.dto.NegozioDto;
 import com.objectmethod.ecommerce.entity.Negozio;
 import com.objectmethod.ecommerce.mapper.NegozioMapper;
 import com.objectmethod.ecommerce.repository.NegozioRepository;
+import com.objectmethod.ecommerce.utils.ResponseWrapper;
 
 public class NegozioService {
 	
@@ -22,5 +24,27 @@ public class NegozioService {
 		List<NegozioDto> dto = negozioMapper.toDto(lst);
 		return dto;
 	}
+	public ResponseEntity<?> FindByCodice(Long codice) {
+		
+		NegozioDto negozioConCodice = negozioMapper.toDto(negozioRepo.findById(codice).get());
+		if(negozioConCodice == null) {
+			return (ResponseEntity<?>) ResponseEntity.badRequest();
+		}else {
+								}
+		return ResponseEntity.ok(new ResponseWrapper<NegozioDto>(negozioConCodice));
+		}
+	public ResponseEntity<?> FindById(Long id) {
+		
+		NegozioDto negozioConId = negozioMapper.toDto(negozioRepo.findById(id).get());
+		if(negozioConId == null) {
+			return (ResponseEntity<?>) ResponseEntity.badRequest();
+		}else {
+								}
+		return ResponseEntity.ok(new ResponseWrapper<NegozioDto>(negozioConId));
+		}
 
-}
+		
+		
+	}
+
+
