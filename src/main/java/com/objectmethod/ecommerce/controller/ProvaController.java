f75e9c4abdbebc218a40d4dbb70f0f6b7d0f8424
maiatteo <97740632+maiatteo@users.noreply.github.com>
2022-03-29 14:26:06 +0200

    Controller  incorporati

--- src/main/java/com/objectmethod/ecommerce/controller/ProvaController.java
+++ src/main/java/com/objectmethod/ecommerce/controller/ProvaController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.objectmethod.ecommerce.ApiGiacenzeResponse;
+import com.objectmethod.ecommerce.dto.utils.ApiBuyBody;
+import com.objectmethod.ecommerce.dto.utils.StorePruductResponse;
 import com.objectmethod.ecommerce.entity.Siu;
 import com.objectmethod.ecommerce.entity.StoreProva;
 
@@ -26,29 +28,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/giacenze")
 public class ProvaController {
 
-
-
-	@GetMapping("/e")
-	public Siu prova() {
-		RestTemplate response = new RestTemplate();
-
-		final String uri = "http://jsonplaceholder.typicode.com/posts/9";
-
-		Siu result = response.getForObject(uri, Siu.class);
-		return result;
-
-	}
-
-	//	@Autowired
-	//	private WebClientCustomizer.Builder webClientBuilder;
-	//
-	//	@GetMapping("/e")
-	//	public Object getPost() {		
-	//	}
+	final String urlBase = "http://192.168.1.192:8090/api/giacenze";
+	
 	@GetMapping("/store/{storeCode}")
 		public ApiGiacenzeResponse store(@PathVariable("storeCode") String storeCode) {
 		RestTemplate response = new RestTemplate();
-		final String url= "http://192.168.1.192:8090/api/giacenze/store/" + storeCode;
+		final String url= urlBase + "/store/" + storeCode;
 		ApiGiacenzeResponse result =response.getForObject(url,ApiGiacenzeResponse.class);
 	
 		return result;
@@ -57,23 +42,38 @@ public class ProvaController {
 	
 	
 	@PostMapping("/buy")
-	public StoreProva buy(StoreProva body) {
+	public ApiGiacenzeResponse buy(@RequestBody List<ApiBuyBody> body) {
 		RestTemplate response = new RestTemplate();
-		final String url ="http://192.168.1.192:8090/api/giacenze/buy";
-		StoreProva result = response.postForObject(url, body, StoreProva.class);
-		return result;
-		
+		final String url = urlBase + "/buy";
+		 ApiGiacenzeResponse result = response.postForObject(url, body, ApiGiacenzeResponse.class);
+		return result;}
+	
+	@GetMapping("/product/{productCode}")
+	public ApiGiacenzeResponse product(@PathVariable("productCode") String productCode) {
+	RestTemplate response = new RestTemplate();
+	final String url = urlBase + "/product/" + productCode;
+	ApiGiacenzeResponse result =response.getForObject(url,ApiGiacenzeResponse.class);
+	return result;
+	}
+	
+	@GetMapping("/stores/{storeCode}/product/{productCode}")
+	public StorePruductResponse storeProduct(@PathVariable("productCode") String productCode,@PathVariable("storeCode") String storeCode) {
+	RestTemplate response = new RestTemplate();
+	final String url= urlBase + "/store/" + storeCode + "/product/" + productCode;
+	StorePruductResponse result =response.getForObject(url,StorePruductResponse.class);
+	return result;
 	}
 	
 	@GetMapping("/stores")
 	public ResponseEntity<ApiGiacenzeResponse> stores(@RequestBody List<String> body) {
 	RestTemplate response = new RestTemplate();
 	HttpEntity<List<String>> request = new HttpEntity<>(body);
-	final String url= "http://192.168.1.192:8090/api/giacenze/stores/" ;
-	ResponseEntity<ApiGiacenzeResponse> result = response.exchange(url,HttpMethod.GET, request ,ApiGiacenzeResponse.class);
+	final String url= urlBase + "/stores" ;
+	ResponseEntity<ApiGiacenzeResponse> result = response.exchange(url,HttpMethod.GET, request ,ApiGiacenzeResponse.class, body);
 	return result;
 	}
 }
 
+	
 
 
