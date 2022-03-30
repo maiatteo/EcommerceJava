15798d9601109f0445b13368d3ec76f4e3451881
Kristian <a@a.a>
2022-03-30 10:05:53 +0200

    find like

--- src/main/java/com/objectmethod/ecommerce/repository/ProdottoRepository.java
+++ src/main/java/com/objectmethod/ecommerce/repository/ProdottoRepository.java
@@ -1,14 +1,12 @@
 package com.objectmethod.ecommerce.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.objectmethod.ecommerce.entity.Carrello;
 import com.objectmethod.ecommerce.entity.Prodotto;
 
 @Repository
@@ -18,4 +16,8 @@ public interface ProdottoRepository extends JpaRepository<Prodotto, Long>{
 	@Modifying
 	@Query(value = "INSERT INTO e_carrelloprodotti(id_prodotti, id_carrello, quantita) VALUES (?1, ?2, ?3)", nativeQuery = true)
 	public void aggiungiAlCarrello(Long idProdotto, Long idCarrello, Integer quantita);
+	
+	@Query(value = "select from e_prodotti where nome like :name", nativeQuery = true)
+	public List<Prodotto> findByNsme(String name);
+	
 }
